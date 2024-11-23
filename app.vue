d0e72e6a2e77e18484de89b3ea7681597dc48b12
AchecheAchraf <achecheachraf@gmail.com>
2024-11-23 14:54:03 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,74 @@
+<template>
+
+  <div
+    class="dark:bg-[url('/assets/bg-cloud-dark.jpg')]  w-screen   bg-[url('/assets/bg-cloud-white.png')] dark:text-white text-black">
+    <TopNavBar></TopNavBar>
+    <NuxtRouteAnnouncer />
+
+    <section id="section1"
+      class="flex overflow-hidden flex-col justify-center items-center dark:bg-[url('/assets/bg-cloud-dark.jpg')] min-h-screen h-auto w-screen bg-cover bg-center bg-[url('/assets/bg-cloud-white.png')] dark:text-white text-black">
+
+      <div class="flex flex-col justify-center items-center w-full h-full font-serif">
+
+        <IntroductionText> </IntroductionText>
+
+
+      </div>
+    </section>
+
+
+
+    <section id="section2"
+      class="flex flex-col sm:flex-row justify-center items-center min-h-screen h-auto w-screen bg-cover bg-center  ">
+      <IntroductionPersonne></IntroductionPersonne>
+    </section>
+
+    <section id="section3" class="flex flex-col justify-center items-center min-h-screen h-auto w-screen">
+      <Skills />
+
+    </section>
+
+    <section id="section4"
+      class="flex flex-col justify-center items-center  bg-pink-50 min-h-screen h-auto w-screen dark:bg-slate-950">
+      <Projects></Projects>
+    </section>
+
+    <section id="section4"
+      class="flex flex-col justify-center items-center  bg-pink-50 h-screen  w-screen dark:bg-slate-950">
+      <Contact></Contact>
+    </section>
+  </div>
+
+
+</template>
+
+<script lang="ts" setup>
+const handleScroll = (event: WheelEvent) => {
+  event.preventDefault(); // Prevent default scroll behavior
+  const sections = document.querySelectorAll('section');
+  let currentSection = Math.round(window.scrollY / window.innerHeight);
+
+  // Handle scroll up
+  if (event.deltaY < 0 && currentSection > 0) {
+    window.scrollTo({
+      top: (currentSection - 1) * window.innerHeight,
+      behavior: 'smooth',
+    });
+  }
+  // Handle scroll down
+  else if (event.deltaY > 0 && currentSection < sections.length - 1) {
+    window.scrollTo({
+      top: (currentSection + 1) * window.innerHeight,
+      behavior: 'smooth',
+    });
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('wheel', handleScroll, { passive: false });
+});
+
+
+</script>
+
+<style scoped></style>
---
+++ components/TopNavBar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav class="sticky top-0 left-0 right-0 z-50 flex flex-row shadow-lg backdrop-blur-sm bg-white/50 dark:bg-slate-800/50 h-[8vh] items-center  justify-center w-screen gap-2">
+    <div class="basis-1/2 flex justify-start items-center text-[4vw] font-serif text-black dark:text-white ml-[7vw]"><a>ⱯA</a></div>
+    <ul class="basis-1/2  flex justify-end items-center flex-row mr-[7vw]">
+        <li class="text-[2vw]  text-black dark:text-white"><button @click="toggleDarkMode">
+          <Icon name="line-md:moon-filled-alt-loop" class="w-[3vw] h-[4vh]" />
+        </button></li>
+    </ul>
+</nav>
+</template>
+
+<script setup>
+    const isDark = ref(false);
+
+const toggleDarkMode = () => {
+  isDark.value = !isDark.value;
+  const htmlEl = document.documentElement;
+  if (isDark.value) {
+    htmlEl.classList.add('dark');
+  } else {
+    htmlEl.classList.remove('dark');
+  }
+};
+</script>
